7f2ba9563923b0196e0d576884bb1179006d7052
noNameDS09 <nonameds1022@gmail.com>
2025-04-27 09:46:50 +0530

    Portfolio

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Amatic_SC } from "next/font/google";
+import { div } from "motion/react-client";
+
+const amatic_SC = Amatic_SC({
+    weight: "700",
+    subsets: ["latin"],
+});
+
+const Hero = () => {
+    return (
+        <div className="p-10">
+            <div className={`${amatic_SC.className} text-white text-5xl`}>
+                Shreyash Daware
+            </div>
+            <div className={`${amatic_SC.className} text-2xl tracking-wider`}>
+                <span>
+                    Hi, I'm Shreyash, a frontend developer who enjoys building
+                    clean and responsive websites. I work with HTML, CSS,
+                    JavaScript, and React to create user-friendly web
+                    experiences. I'm always learning and love turning ideas into
+                    real, functional designs.
+                </span>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
