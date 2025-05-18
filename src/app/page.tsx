a0ed9a7d1b2beb0e2e78bdd3f3cd3a42d1cee867
noNameDS09 <nonameds1022@gmail.com>
2025-05-18 10:11:43 +0530

    #

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,21 @@ import Contact from "./components/Hero/Contact";
 import Projects from "./components/Projects/Projects";
 import { TechStack } from "./components/TechStack/Techstack";
 import Cursor from "./components/CustomCursor/Cursor";
-import StickyCursor from "./components/CustomCursor/StickyCursor/MagneticCursor";
+// import StickyCursor from "./components/CustomCursor/StickyCursor/MagneticCursor";
 import "../app/globals.css";
 import Image from "next/image";
 const Page = () => {
-  const stickyElement = useRef(null);
+  // const stickyElement = useRef<HTMLElement | null>(null);
   return (
     <div className="bg-[#0e0e0e]">
       <Cursor />
       <section className=" ">
         <nav>
           <Heading />
-          <Contact ref={stickyElement} />
-            <StickyCursor stickyElement={stickyElement} />
+          {/* <Contact ref={stickyElement} /> */}
+            {/* <StickyCursor stickyElement={stickyElement} /> */}
+            <Contact />
+
         </nav>
         <main className="flex justify-end items-center h-screen w-screen px-20">
           <Image height={500} width={1000} src={"/DevilGirl.jpg"} alt="image" className="rounded-br-4xl rounded-bl-full rounded-tr-none"/>
