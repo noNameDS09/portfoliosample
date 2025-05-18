c817ba477e6b51900c108848c237747862782a34
noNameDS09 <nonameds1022@gmail.com>
2025-05-18 12:39:45 +0530

    any

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
 "use client";
-import React from "react";
+import React, { useRef } from "react";
 import Heading from "./components/Hero/Heading";
 import Contact from "./components/Hero/Contact";
 // import { Hero } from "./components/Hero/Hero";
 import Projects from "./components/Projects/Projects";
 import { TechStack } from "./components/TechStack/Techstack";
 import Cursor from "./components/CustomCursor/Cursor";
-// import StickyCursor from "./components/CustomCursor/StickyCursor/MagneticCursor";
+import StickyCursor from "./components/CustomCursor/StickyCursor/MagneticCursor";
 import "../app/globals.css";
 import Image from "next/image";
 const Page = () => {
-  // const stickyElement = useRef<HTMLElement | null>(null);
+  const stickyElement = useRef<any | null>(null);
   return (
     <div className="bg-[#0e0e0e]">
       <Cursor />
       <section className=" ">
         <nav>
           <Heading />
-          {/* <Contact ref={stickyElement} /> */}
-            {/* <StickyCursor stickyElement={stickyElement} /> */}
+          <Contact ref={stickyElement} />
+            <StickyCursor stickyElement={stickyElement} />
             <Contact />
 
         </nav>
