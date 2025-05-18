7a957743ac02d4821053219392c8e3d42abde206
noNameDS09 <nonameds1022@gmail.com>
2025-05-18 10:12:48 +0530

    #

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useRef } from "react";
+import React from "react";
 import Heading from "./components/Hero/Heading";
 import Contact from "./components/Hero/Contact";
 // import { Hero } from "./components/Hero/Hero";
